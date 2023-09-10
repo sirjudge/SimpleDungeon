7a1d1eb6709806d211d630a74c84d61c3edfdeb6
nico <nico.a.judge@gmail.com>
2023-09-10 19:12:41 -0400

    made some namespace and function definition changes

--- startUp.ts
+++ startUp.ts
@@ -1,10 +1,20 @@
+import AuthenticateUser from "./backend/Authenticate";
+
 const server = Bun.serve({
-      // routing logic
   port: 3000,
   fetch(request) {
-      const url = new URL(request.url);
-      console.log(url);
-      switch (url.pathname){
+    AuthenticateUser(request);
+    Route(request);
+    return Route(request);
+  },
+});
+
+
+
+function Route(request: Request){
+    const url = new URL(request.url);
+    console.log(url);
+    switch (url.pathname){
         case "/": 
             return new Response(Bun.file(import.meta.dir + "/frontend/pages/home.html"));
         case "/launchGame":
@@ -12,12 +22,9 @@ const server = Bun.serve({
         case "/connectToGame":
             return new Response(Bun.file(import.meta.dir + "/frontend/pages/connectToGame.html"));
         case "/about":
-            return new Response(Bun.file(import.meta.dir + "frontend/pages/about.html"));
+            return new Response(Bun.file(import.meta.dir + "/frontend/pages/about.html"));
         default:
             return new Response("404 Not Found", { status: 404 });
-      }
-  },
-});
+    }
+}
 
-
-console.log("server is running on port ", server.port);
